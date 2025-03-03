d6af7a5dbf060eb5102835e33ae0c05d1e607a4e
Tristan Bosman <u23614286@tuks.co.za>
2025-03-03 22:07:55 +0200

    INF272_Practical1_u23614286

---
+++ TristanStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TristanStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult ListStudents()
+        {
+            //List using the student model
+            List<Models.Student> students = new List<Models.Student>
+            {
+                //values for properties to be used in study group table
+                new Models.Student { StudentNumber = "u23614286", FirstName = "Tristan", LastName = "Bosman", Email = "u23614286@tuks.co.za" },
+                new Models.Student { StudentNumber = "u23541416", FirstName = "Tayla", LastName = "Walther", Email = "u23541416@tuks.co.za" },
+                new Models.Student { StudentNumber = "u23543681", FirstName = "Tia", LastName = "Naidoo", Email = "u23543681@tuks.co.za" },
+                new Models.Student { StudentNumber = "u24650677", FirstName = "Ashton", LastName = "Theron", Email = "u24650677@tuks.co.za" },
+                new Models.Student { StudentNumber = "u24924718", FirstName = "Gundo", LastName = "Ndou", Email = "u24924718@tuks.co.za" }
+            };
+            //return the view
+            return View(students);
+        }
+    }
+}
---
+++ TristanStudyGroup/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TristanStudyGroup.Models
+{
+	//Class for student to be used for mvc
+	public class Student
+	{
+        //property and decorator for student num
+		[Display(Name = "Student Number")]
+		public string StudentNumber { get; set; }
+        //property and decorator for Name
+        [Display(Name = "Name")]
+        public string FirstName { get; set; }
+        //property and decorator for Surname
+        [Display(Name = "Surname")]
+        public string LastName { get; set; }
+        //property and decorator for email address
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+    }
+}
